b4be1bb382d69771ced06b8060ae0966232b292b
ignaci0 <ibutler@gmail.com>
2018-11-01 18:22:50 +0100

    Fixed module description format

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Treex.MixProject do
   def project do
     [
       app: :treex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -31,7 +31,7 @@ defmodule Treex.MixProject do
 
   defp description() do
     """
-    An Elixir wrapper to the Erlang's `:gb_trees` module.
+    An Elixir wrapper to the Erlang's :gb_trees module.
 
     It also provides stream/1 on top
     """
